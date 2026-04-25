1e21855b07397446bb1948015c32247988099533
Patipuu <147331118+Patipuu@users.noreply.github.com>
2026-04-25 09:04:04 +0700

    fiix

--- internal/middleware/maintenance_middleware.go
+++ internal/middleware/maintenance_middleware.go
@@ -10,19 +10,32 @@ func MaintenanceMiddleware(isMaintenance bool) func(http.Handler) http.Handler {
 		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 			if isMaintenance {
 				w.Header().Set("Content-Type", "application/json")
-				w.WriteHeader(http.StatusServiceUnavailable) 
+				w.WriteHeader(http.StatusServiceUnavailable) // 503
 				json.NewEncoder(w).Encode(map[string]string{
 					"message": "Hệ thống đang bảo trì, vui lòng quay lại sau.",
 				})
-				
-				return 
+				return
 			}
-			w.Header().Set("Content-Type", "application/json")
-			w.WriteHeader(http.StatusOK) // 200
+			// isMaintenance = false -> cho request đi qua bình thường
+			next.ServeHTTP(w, r)
+		})
+	}
+}
+
+
+func MaintenanceHandler(isMaintenance bool) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Content-Type", "application/json")
+		if isMaintenance {
+			w.WriteHeader(http.StatusServiceUnavailable) // 503
 			json.NewEncoder(w).Encode(map[string]string{
-				"data": "Chào mừng đến với hệ thống đặt vé!",
+				"message": "Hệ thống đang bảo trì, vui lòng quay lại sau.",
 			})
 			return
+		}
+		w.WriteHeader(http.StatusOK) // 200
+		json.NewEncoder(w).Encode(map[string]string{
+			"data": "Chào mừng đến với hệ thống đặt vé!",
 		})
 	}
 }

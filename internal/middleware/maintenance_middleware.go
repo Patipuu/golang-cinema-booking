80a4a32df61e955b2a49157ce061f93859aac3dd
Patipuu <147331118+Patipuu@users.noreply.github.com>
2026-04-25 08:39:54 +0700

    fĩ

--- internal/middleware/maintenance_middleware.go
+++ internal/middleware/maintenance_middleware.go
@@ -14,9 +14,15 @@ func MaintenanceMiddleware(isMaintenance bool) func(http.Handler) http.Handler {
 				json.NewEncoder(w).Encode(map[string]string{
 					"message": "Hệ thống đang bảo trì, vui lòng quay lại sau.",
 				})
+				
 				return 
 			}
-			next.ServeHTTP(w, r)
+			w.Header().Set("Content-Type", "application/json")
+			w.WriteHeader(http.StatusOK) // 200
+			json.NewEncoder(w).Encode(map[string]string{
+				"data": "Chào mừng đến với hệ thống đặt vé!",
+			})
+			return
 		})
 	}
 }
